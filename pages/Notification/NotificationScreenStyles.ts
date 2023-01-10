db8cb2271ab77f9c12e9992d57741fd268acfa41
Olayiwola Ayoola <46034532+Layoolar@users.noreply.github.com>
2023-01-10 23:12:27 +0100

    Add files via upload

---
+++ pages/Notification/NotificationScreenStyles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  circle: {
+    width: 150,
+    height: 150,
+    backgroundColor: 'red',
+    borderRadius: 75,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 25
+  },
+})
---
+++ pages/Picture/PictureScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const PictureScreen = () => {
+
+    return (
+      <View>
+        <Text> Picture </Text>
+      </View>
+    )
+}
+
+export default PictureScreen

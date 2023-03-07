fcc112ac267ed3e1dc6a463db157733857fd87c5
Olayiwola Ayoola <46034532+Layoolar@users.noreply.github.com>
2023-03-07 16:14:24 +0100

    Update firebase.tsx

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -8,13 +8,13 @@ import { initializeFirestore, getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAwvkCHlvnPB6Qe8xpF16kUJHSxLh1tP6w",
-  authDomain: "myreact-ba182.firebaseapp.com",
-  projectId: "myreact-ba182",
-  storageBucket: "myreact-ba182.appspot.com",
-  messagingSenderId: "26100785288",
-  appId: "1:26100785288:web:dd004be059459aa5a43f3d",
-  measurementId: "G-W3JPZN0XMW"
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ",
+  appId: ,
+  measurementId: 
 };
 
 const app = initializeApp(firebaseConfig);
